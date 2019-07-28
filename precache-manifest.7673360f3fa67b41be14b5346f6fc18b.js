0aae6fe244afdaecb597e13691e5de329241afbb
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-07-28 14:32:13 -0700

    Updates

---
+++ precache-manifest.7673360f3fa67b41be14b5346f6fc18b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144b98348483fdaa5dc820f79e5d4f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f088222cca3f0b0be4",
+    "url": "/static/css/main.0a4b2e2b.chunk.css"
+  },
+  {
+    "revision": "c3b0e602a426effcea41",
+    "url": "/static/js/2.3f93c43f.chunk.js"
+  },
+  {
+    "revision": "a8f088222cca3f0b0be4",
+    "url": "/static/js/main.120316b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
